31b31994dfb704578351edd568f082bd0328360b
username-Hello <nikitos.burak@gmail.com>
2021-08-26 22:46:41 +0300

    add http post method

--- src/test/java/com/breweryclient/web/client/BeerClientTest.java
+++ src/test/java/com/breweryclient/web/client/BeerClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +20,13 @@ class BeerClientTest {
     void getBeerById() {
         BeerDto beerDto = beerClient.getBeerById(UUID.randomUUID());
         assertNotNull(beerDto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        BeerDto beerDto = new BeerDto();
+        URI uri = beerClient.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri);
     }
 }
